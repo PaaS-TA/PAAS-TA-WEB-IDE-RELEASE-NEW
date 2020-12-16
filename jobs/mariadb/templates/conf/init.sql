cb8e14208cd0cd9f1cf8ddcbe43decdc981d0eef
82105 <82105@DESKTOP-EE0FLSP>
2020-12-17 00:25:31 +0900

    OnDemand 설정 및 테이블 스크립트 추가

--- jobs/mariadb/templates/conf/init.sql
+++ jobs/mariadb/templates/conf/init.sql
@@ -36,3 +36,24 @@ CREATE TABLE `web_ide_service_list` (
   `web_ide_service` varchar(128) NOT NULL,
   PRIMARY KEY (`no`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for on_demand_info
+-- ----------------------------
+DROP TABLE IF EXISTS `on_demand_info`;
+CREATE TABLE `on_demand_info`  (
+  `service_instance_id` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `dashboard_url` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `task_id` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `use_yn` varchar(1) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT 'Y',
+  `plan_id` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `service_id` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `space_guid` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `organization_guid` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `vm_instance_id` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `app_guid` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `app_parameter` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`service_instance_id`) USING BTREE
+) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
