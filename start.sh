4b5a06862e19e262e1c519e7681469df8836ba4f
okpc579 <55691511+okpc579@users.noreply.github.com>
2021-02-16 11:13:19 +0900

    2.0 -> 2.0.0

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-bosh create-release --sha2 --force --tarball ./paas-ta-webide-release-2.0.tgz --name paas-ta-webide-release --version 2.0
+bosh create-release --sha2 --force --tarball ./paas-ta-webide-release-2.0.0.tgz --name paas-ta-webide-release --version 2.0.0
 
 
-bosh upload-release ./paas-ta-webide-release-2.0.tgz
+bosh upload-release ./paas-ta-webide-release-2.0.0.tgz
