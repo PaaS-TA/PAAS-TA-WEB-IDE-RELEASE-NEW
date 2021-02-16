7ccc8d3d0d0f68c4cc6ce1890355c001d5541529
PaaS-TA <paastaguile@gmail.com>
2021-02-16 02:05:40 +0000

    start.sh 1.1.0 upgrade

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-bosh create-release --sha2 --force --tarball ./paas-ta-webide-release-1.0.tgz --name paas-ta-webide-release --version 1.0
+bosh create-release --sha2 --force --tarball ./paas-ta-webide-release-1.1.0.tgz --name paas-ta-webide-release --version 1.1.0
 
 
-bosh upload-release ./paas-ta-webide-release-1.0.tgz
+bosh upload-release ./paas-ta-webide-release-1.1.0.tgz
