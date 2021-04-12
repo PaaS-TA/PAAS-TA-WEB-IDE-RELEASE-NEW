e1b0aeb7eb0369fd0fbe3b071cd4b7ff7904b9ec
okpc579 <55691511+okpc579@users.noreply.github.com>
2021-04-12 12:55:27 +0900

    Update init.sql

--- jobs/mariadb/templates/conf/init.sql
+++ jobs/mariadb/templates/conf/init.sql
@@ -1,4 +1,4 @@
-<% if p("cce_enable") == "false" %>
+<% if p("cce_enable") == false %>
     UPDATE mysql.user SET password=PASSWORD('<%= p("mariadb.admin_user.password") %>') WHERE user='root';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '<%= p("mariadb.admin_user.password") %>' WITH GRANT OPTION;
 <% end %>
